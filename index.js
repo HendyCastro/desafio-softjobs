1e19146a4a427f201166f07f88fe4a376ad669d6
HendyCastro <hendy0702@gmail.com>
2024-05-23 15:59:20 -0400

    subiendo verificacion de credenciales

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const { pool } = require("./database/index.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("servidor iniciado en el puerto 3000");
+});
 
 app.post("/usuarios", async (req, res) => {
     try {
@@ -22,9 +27,34 @@ app.post("/usuarios", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("servidor iniciado en el puerto 3000");
-});
+app.post("/login", async (req, res) => {    
+   try {
+    const { email, password } = req.body;
+    const query = "SELECT * FROM usuarios WHERE email = $1;";
+    const values = [email];
+    const { rows } = await pool.query(query, values);
+    if (rows.length === 0) {
+        return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+    const user = rows[0];
+    if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+    const key = "llaveSecreta";
+
+    const token = jwt.sign({ email: user.email, rol: user.rol, lenguage: user.lenguage },
+         key
+    );
+
+    res.status(200).json(token);
+   } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+    
+}
+    });
+
+
 
 
 
