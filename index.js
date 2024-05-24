72f67fc7928bd0225d7b0d92f30d5230b683c104
HendyCastro <hendy0702@gmail.com>
2024-05-24 14:23:19 -0400

    subiendo modificaciones

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const app = express();
 
+const key = "claveSecreta";
 app.use(cors());
 app.use(express.json());
 
 app.listen(3000, () => {
     console.log("servidor iniciado en el puerto 3000");
 });
+
+const verifyToken = (req, res, next) => {
+    const authorizationHeader = req.headers["authorization"];
+    if (!authorizationHeader) {
+        return res.status(401).json({ message: "No autorizado" });
+    }
+    const [bearer, token] = authorizationHeader.split(" ");
+
+    if (bearer !== "Bearer" || !token) {
+        return res.status(401).json({ message: "No autorizado" });    
+    }
+    try {
+        jwt.verify(token, key)&&next();
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ message: "No autorizado!!!" });
+    }
+};
 
 app.post("/usuarios", async (req, res) => {
     try {
@@ -40,19 +60,37 @@ app.post("/login", async (req, res) => {
     if (!bcrypt.compareSync(password, user.password)) {
         return res.status(401).json({ message: "Credenciales inválidas" });
     }
-    const key = "llaveSecreta";
 
     const token = jwt.sign({ email: user.email, rol: user.rol, lenguage: user.lenguage },
          key
+         
     );
-
     res.status(200).json(token);
    } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
-    
 }
-    });
+});
+
+app.get("/usuarios", verifyToken, async (req, res) => {
+    
+    try {
+        const [_,token] = req.headers.authorization.split(" ");
+        const query = "SELECT * FROM usuarios WHERE email = $1;";
+        const {email}= jwt.verify(token, key);
+        const { rows } = await pool.query(query, [email]);
+        const user = rows[0];
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error al obtener los usuarios" });
+        
+    }
+});
 
 
 
